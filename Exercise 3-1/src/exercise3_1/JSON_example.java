4f3ba904db2058a35736e32a9987ec65db00faaa
Osiris <osiris.villa.ca@gmail.com>
2019-05-24 12:17:37 -0500

    Little bit of formating

--- Exercise 3-1/src/exercise3_1/JSON_example.java
+++ Exercise 3-1/src/exercise3_1/JSON_example.java
@@ -6,39 +6,32 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class JSON_example {
-	
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		JSONParser parser = new JSONParser();
-		 
-        try {
-        	
-        	FileReader reader = new FileReader("/Users/hector.villa/eclipse-workspace/udemy_course/Exercise 3-1/json_sdet.json");
- 
-            Object obj = parser.parse(reader);
-            
-           
-      
-            JSONObject jsonObject = (JSONObject) obj;
-            
-            JSONObject oli = (JSONObject) jsonObject.get("AppSettings");
-          
- 
-            String BaseUrl = (String) oli.get("BaseUrl");
-            String BrowserTarget = (String) oli.get("BrowserTarget");
-           
- 
-            System.out.println("BaseUrl: " + BaseUrl);
-            System.out.println("BrowserTarget: " + BrowserTarget);
-            
-          
-            
- 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+		try {
+
+			FileReader reader = new FileReader(
+					"/Users/hector.villa/eclipse-workspace/udemy_course/Exercise 3-1/json_sdet.json");
+
+			Object obj = parser.parse(reader);
+
+			JSONObject jsonObject = (JSONObject) obj;
+
+			JSONObject oli = (JSONObject) jsonObject.get("AppSettings");
+
+			String BaseUrl = (String) oli.get("BaseUrl");
+			
+			String BrowserTarget = (String) oli.get("BrowserTarget");
+
+			System.out.println("BaseUrl: " + BaseUrl);
+			System.out.println("BrowserTarget: " + BrowserTarget);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
